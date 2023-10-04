b25bb29ef016fafcb64494a1f451c20c807a2636
Klajdi <klajdixhafkollari36@gmail.com>
2023-10-04 15:52:48 +0200

    chngs2

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Layout } from 'antd';
 import Navbar from './components/common/Navbar';
-import './App.css';
+// import './App.css';
+import SignUp from './components/common/SignUp';
+import "./assets/styles/main.css";
+import "./assets/styles/responsive.css";
+// import "antd/dist/reset.css";
 
 const App = () => {
   return (
-    <Layout className="w-screen min-h-screen bg-richblack-900 flex flex-col fom=nt-inter">
-      <Navbar/>
-    </Layout>
+    // <Layout className="w-screen min-h-screen bg-richblack-900 flex flex-col fom=nt-inter">
+    //   <Navbar/>
+    // </Layout>
+    <SignUp/>
   );
 };
 
