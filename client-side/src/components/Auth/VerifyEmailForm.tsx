305bb0df69629884b3980f16da51900f22d11ded
Klajdi <klajdixhafkollari36@gmail.com>
2023-12-21 17:24:38 +0100

    chngs2

--- client-side/src/components/Auth/VerifyEmailForm.tsx
+++ client-side/src/components/Auth/VerifyEmailForm.tsx
@@ -1,16 +1,38 @@
-import { FunctionComponent, useState } from "react";
+import { FunctionComponent, useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import OtpInput from "react18-input-otp";
-import { Form, Button } from "antd";
+import { Form, Button, Progress } from "antd";
 
 const VerifyEmailForm: FunctionComponent = () => {
     const [code, setCode] = useState<string>("");
+    const [secondsLeft, setSecondsLeft] = useState<number>(60);
+    const [progressColor, setProgressColor] = useState<string>('#EB6536');
+
 
     const handleOtpChange = async (code: string) => {
         setCode(code);
     };
 
     const handleOnSubmit = async () => {};
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setSecondsLeft((prevSeconds) => {
+                if (prevSeconds > 0) {
+                    if (prevSeconds <= 15) {
+                        setProgressColor('#cf1322');
+                    }
+                    return prevSeconds - 1;
+                } else {
+                    clearInterval(timer);
+                    return 0;
+                }
+            });
+        }, 1000);
+
+        return () => clearInterval(timer);
+    }, []);
+
 
     return (
         <Form 
@@ -49,6 +71,18 @@ const VerifyEmailForm: FunctionComponent = () => {
             >
                 Verify Email
             </Button>
+            {secondsLeft > 0 &&
+                <Progress
+                    percent={(secondsLeft / 60) * 100}
+                    steps={5}
+                    className="mt-4"
+                    size={20}
+                    strokeColor={progressColor}
+                    format={() => (
+                        <span style={{ color: '#EB6536' }}>{`${secondsLeft}s`}</span>
+                    )}
+                />
+            }
             <div className="flex justify-center text-[#F1F2FF] mt-4">
                 Can't get OTP Code? &nbsp;
                 <Link to="/signup">
