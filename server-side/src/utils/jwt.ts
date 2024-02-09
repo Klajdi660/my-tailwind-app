08ad05bb1078413537ab9cbd053fbbaf1da25a6c
Klajdi <klajdixhafkollari36@gmail.com>
2024-02-09 16:10:45 +0100

    chngs2

--- server-side/src/utils/jwt.ts
+++ server-side/src/utils/jwt.ts
@@ -28,7 +28,6 @@ const signJWT = (
 };
 
 export const signToken = async (user: any) => {
-    console.log('user :>> ', user);
     const access_token = signJWT({ id: user.id }, "accessTokenPrivateKey", {
         expiresIn: `${access_token_expires}d`,
     });
