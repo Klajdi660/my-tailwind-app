9683489034ccd8fad50b8b4a57972ca69af7c81b
Klajdi <klajdixhafkollari36@gmail.com>
2024-04-18 17:28:43 +0200

    chngs2

--- client-side/src/contexts/AuthContext.tsx
+++ client-side/src/contexts/AuthContext.tsx
@@ -29,17 +29,17 @@ const AuthProvider: FunctionComponent<AuthProviderProps> = ({ children }) => {
 
   useEffect(() => {
     if (localStorage.atoken) {
-      // const extraParse = JSON.parse(JSON.parse(localStorage.user).extra);
-      // const userParse = JSON.parse(localStorage.user);
-      // const firstNameInitial = extraParse?.firstName?.charAt(0) || "";
-      // const lastNameInitial = extraParse?.lastName?.charAt(0) || "";
-      // const name = `${firstNameInitial}${lastNameInitial}`;
-      // const userAvatar = extraParse.photos;
+      const extraParse = JSON.parse(JSON.parse(localStorage.user).extra);
+      const userParse = JSON.parse(localStorage.user);
+      const firstNameInitial = extraParse?.firstName?.charAt(0) || "";
+      const lastNameInitial = extraParse?.lastName?.charAt(0) || "";
+      const name = `${firstNameInitial}${lastNameInitial}`;
+      const userAvatar = extraParse.photos;
       // ? extraParse.photos
       // : `https://place-hold.it/52x52/F3F4F6/4B5563&text=${firstNameInitial}${lastNameInitial}&fontsize=20`;
-      // setUser({ ...userParse, avatar: userAvatar, extra: extraParse, name });
+      setUser({ ...userParse, avatar: userAvatar, extra: extraParse, name });
     }
-    if (localStorage.atoken) setUser({ id: JSON.parse(localStorage.user).id });
+    // if (localStorage.atoken) setUser({ id: JSON.parse(localStorage.user).id });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
