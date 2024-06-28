772ec78bda8580c2706cbb085d5885e29cfa031f
Klajdi Xhafkollari <klajdixhafkollari36@gmail.com>
2024-06-28 14:40:59 +0200

    chbgs3

--- client-side/src/layouts/PrivateLayout.tsx
+++ client-side/src/layouts/PrivateLayout.tsx
@@ -1,10 +1,14 @@
 import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { CartSwitcher, Loading, Navbar, Sidebar, TopPlay } from "../components";
+import { useAuth } from "../hooks";
 import { useUserService } from "../services";
 import { ProviderProps } from "../types";
 
 export const PrivateLayout = ({ children, ...restProps }: ProviderProps) => {
   const { getUserDetails } = useUserService();
+  const { user } = useAuth();
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchUserDetails = async () => {
@@ -18,6 +22,12 @@ export const PrivateLayout = ({ children, ...restProps }: ProviderProps) => {
     fetchUserDetails();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  useEffect(() => {
+    if (user) {
+      navigate("/discover");
+    }
+  }, [user]);
 
   return (
     <div
