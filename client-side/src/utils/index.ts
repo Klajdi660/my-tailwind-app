82f35c612e89a264361bcce32f09f36c7ece7162
Klajdi Xhafkollari <klajdixhafkollari36@gmail.com>
2024-07-08 11:36:48 +0200

    chngs2

--- client-side/src/utils/index.ts
+++ client-side/src/utils/index.ts
@@ -1,5 +1,5 @@
 export * from "./validations/auth";
 export * from "./validations/profile";
-export { globalObject } from "./global-object";
 export * from "./helperFunctions";
 export * from "./manageState";
+export { globalObject } from "./global-object";
