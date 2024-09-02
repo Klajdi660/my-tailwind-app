73ab1fa110cdde4c00a510a1329d97d7078b837e
Klajdi660 <klajdixhafkollari36@gmail.com>
2024-09-02 21:30:43 +0200

    fix

--- client-side/src/pages/Root/Home.tsx
+++ client-side/src/pages/Root/Home.tsx
@@ -3,7 +3,7 @@ import { FC, useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { iconName } from "../../assets";
 import { HomePageProps } from "../../types";
-import { Image, Button, HomeFooter } from "../../components";
+import { Image, Button, HomeFooter, PlatformIconList } from "../../components";
 import { useGame, useGameSlider } from "../../hooks";
 import { classNames } from "../../utils";
 
@@ -23,6 +23,7 @@ export const HomePage: FC<HomePageProps> = () => {
 
   const [backgroundImage, setBackgroundImage] = useState<string | undefined>();
   const [selectedGameId, setSelectedGameId] = useState<number | undefined>();
+  const [platformsIcon, setPlatformsIcon] = useState([]);
 
   const { gameDetail } = useGame(selectedGameId) as any;
 
@@ -31,14 +32,22 @@ export const HomePage: FC<HomePageProps> = () => {
       const initialBackgroundImage =
         gamesSlider[0].background_image || undefined;
       const initialGameId = gamesSlider[0].id || undefined;
+      const platforms = gamesSlider[0].parent_platforms || [];
+
       setBackgroundImage(initialBackgroundImage);
       setSelectedGameId(initialGameId);
+      setPlatformsIcon(platforms);
     }
   }, [gamesSlider]);
 
-  const selectedGameHandler = (gameId: number, imgUrl: string) => {
+  const selectedGameHandler = (
+    gameId: number,
+    imgUrl: string,
+    parent_platforms: any
+  ) => {
     setSelectedGameId(gameId);
     setBackgroundImage(imgUrl);
+    setPlatformsIcon(parent_platforms);
   };
 
   if (!gamesSlider) return null;
@@ -68,7 +77,7 @@ export const HomePage: FC<HomePageProps> = () => {
               whileHover={{ scale: 1.1 }}
             >
               <Button
-                className="w-25 bg-white bg-opacity-10 text-white"
+                className="w-24 bg-white bg-opacity-10 text-white"
                 iconClassName="text-white"
                 variant="none"
                 label="Login"
@@ -80,12 +89,16 @@ export const HomePage: FC<HomePageProps> = () => {
         </div>
         <div className="flex h-auto items-center justify-center my-10">
           <div className="flex w-11/12 max-w-full items-center justify-between">
-            <div className="flex flex-row items-start justify-end gap-2">
+            <div className="flex md:flex-row flex-col items-start justify-end gap-2">
               {gamesSlider?.map((game: any, index: number) => (
-                <div key={game.id} className="relative">
+                <div key={game.id} className="relative flex items-end">
                   <button
                     onClick={() =>
-                      selectedGameHandler(game.id, game.background_image)
+                      selectedGameHandler(
+                        game.id,
+                        game.background_image,
+                        game.parent_platforms
+                      )
                     }
                   >
                     <Image
@@ -101,10 +114,20 @@ export const HomePage: FC<HomePageProps> = () => {
                   {selectedGameId === game.id && (
                     <div
                       className={classNames(
-                        "absolute bottom-3 w-[500px]",
-                        index === gamesSlider.length - 1 ? "right-0" : "left-40"
+                        // "absolute bottom-3 flex flex-row items-center gap-2",
+                        "flex flex-row gap-2 items-center",
+                        "fixed ml-40"
+                        // index === gamesSlider.length - 1
+                        //   ? "static mr-40"
+                        //   : "fixed ml-40"
                       )}
                     >
+                      <div className="flex gap-2 p-2 bg-white rounded-md">
+                        <PlatformIconList
+                          className="text-onNeutralBg"
+                          platforms={platformsIcon.map((p: any) => p.platform)}
+                        />
+                      </div>
                       <p className="text-white text-2xl">{gameDetail?.name}</p>
                     </div>
                   )}
