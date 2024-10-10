56ceb59fca5050f2e4335146ad0abc632017e899
Klajdi660 <klajdixhafkollari36@gmail.com>
2024-10-10 21:35:59 +0200

    chngs443

--- client-side/src/components/UI/Image.tsx
+++ client-side/src/components/UI/Image.tsx
@@ -1,16 +1,18 @@
 import { FC } from "react";
 import { ImageProps } from "../../types";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 
 export const Image: FC<ImageProps> = (props) => {
   const { imgUrl, name, width, height, styles } = props;
 
   return (
-    <img
+    <LazyLoadImage
       alt={name}
       src={imgUrl}
       width={width}
       height={height}
       className={styles}
+      effect="blur"
     />
   );
 };
