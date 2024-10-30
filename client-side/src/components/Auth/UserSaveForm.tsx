4af68840f46ef15a41535ca0c7e460a1d1f2f83f
Klajdi660 <klajdixhafkollari36@gmail.com>
2024-10-30 21:13:42 +0100

    fix22

--- client-side/src/components/Auth/UserSaveForm.tsx
+++ client-side/src/components/Auth/UserSaveForm.tsx
@@ -19,30 +19,39 @@ export const UserSaveForm: FC = () => {
           <button
             type="button"
             key={saveAuthUser.id}
-            className="flex_justify_center flex-col text-onNeutralBg bg-card rounded-xl p-4 gap-4 hover:bg-primary-opacity w-40 h-52"
+            className="flex flex-col items-center text-onNeutralBg bg-card rounded-xl p-4 hover:bg-primary-opacity w-44 h-52"
           >
-            <div className="w-full text-center">{saveAuthUser.email}</div>
-            {saveAuthUser.photo ? (
-              <Image
-                imgUrl={saveAuthUser.photo}
-                name="Profile Img"
-                styles="w-20 h-20 rounded-full p-1 ring-2 ring-white object-cover"
-                effect="blur"
+            <div className="flex w-full justify-end">
+              <Icon
+                name="BsThreeDotsVertical"
+                className="text-secondary"
+                size={12}
               />
-            ) : (
-              <Image
-                imgUrl={userIcon}
-                name="Profile Img"
-                styles="w-20 h-20 rounded-full p-1 ring-2 ring-white bg-main"
-                effect="blur"
-              />
-            )}
-            <div className="w-full text-center">{saveAuthUser.username}</div>
+            </div>
+            <div className="flex_justify_between flex-col gap-4 w-full">
+              <p className="w-full text-center">{saveAuthUser.email}</p>
+              {saveAuthUser.photo ? (
+                <Image
+                  imgUrl={saveAuthUser.photo}
+                  name="Profile Img"
+                  styles="w-20 h-20 rounded-full p-1 ring-2 ring-white object-cover"
+                  effect="blur"
+                />
+              ) : (
+                <Image
+                  imgUrl={userIcon}
+                  name="Profile Img"
+                  styles="w-20 h-20 rounded-full p-1 ring-2 ring-white bg-main"
+                  effect="blur"
+                />
+              )}
+              <p className="w-full text-center">{saveAuthUser.username}</p>
+            </div>
           </button>
         );
       })}
       <button
-        className="flex_justify_center flex-col text-onNeutralBg bg-card rounded p-4 gap-4 w-40 h-52 hover:bg-primary-opacity"
+        className="flex_justify_center flex-col text-onNeutralBg bg-card rounded p-4 gap-4 w-44 h-52 hover:bg-primary-opacity"
         onClick={() => navigate(login)}
       >
         <div className="w-full text-center">
@@ -50,7 +59,7 @@ export const UserSaveForm: FC = () => {
           {/* <div className="w-full h-[1px] bg-divider mt-4" /> */}
         </div>
         <Icon name="FaCirclePlus" className="w-20 h-20 text-secondary" />
-        <div className="h-5"></div>
+        <div className="h-4"></div>
       </button>
     </div>
   );
