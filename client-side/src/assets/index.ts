75a648e2d9f97c92862fe841b1e4072a2e769a3b
Klajdi Xhafkollari <klajdixhafkollari36@gmail.com>
2025-07-30 10:47:07 +0200

    dt_30_07_25_chngs_1

--- client-side/src/assets/index.ts
+++ client-side/src/assets/index.ts
@@ -1,20 +1,20 @@
-import icon from "./icon.png";
-import noImg from "./noImg.jpg";
-import bankImg from "./visa.png";
-import userIcon from "./user.png";
-import avatar1 from "./avatar1.png";
-import avatar2 from "./avatar2.png";
-import avatar3 from "./avatar3.png";
-import avatar4 from "./avatar4.png";
-import iconName from "./iconName.png";
-import mobileMoneyImg from "./momo.png";
-import visaCardImg from "./visa.svg.png";
-import emptyCartImg from "./emptyCart.svg";
-import masterCardImg from "./MasterCard.png";
-import maestroCardImg from "./MaestroCard.png";
-import noCardTypeImg from "./no-card-type.png";
-import discoverCardImg from "./DiscoverCard.png";
-import usaExpCardImg from "./AmericanExpressCard.png";
+import icon from "./app/icon.png";
+import noImg from "./app/noImg.jpg";
+import iconName from "./app/iconName.png";
+import emptyCartImg from "./app/emptyCart.svg";
+import bankImg from "./card/visa.png";
+import mobileMoneyImg from "./card/momo.png";
+import visaCardImg from "./card/visa.svg.png";
+import masterCardImg from "./card/MasterCard.png";
+import maestroCardImg from "./card/MaestroCard.png";
+import noCardTypeImg from "./card/no-card-type.png";
+import discoverCardImg from "./card/DiscoverCard.png";
+import usaExpCardImg from "./card/AmericanExpressCard.png";
+import userIcon from "./user/user.png";
+import avatar1 from "./user/avatar1.png";
+import avatar2 from "./user/avatar2.png";
+import avatar3 from "./user/avatar3.png";
+import avatar4 from "./user/avatar4.png";
 
 export {
   icon,
