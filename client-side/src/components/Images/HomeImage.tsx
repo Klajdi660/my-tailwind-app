16f079180c6e3f1eb7e0ba61ed335623c34cd423
Klajdi660 <klajdixhafkollari36@gmail.com>
2026-04-09 21:16:06 +0200

    fix home image

--- client-side/src/components/Images/HomeImage.tsx
+++ client-side/src/components/Images/HomeImage.tsx
@@ -31,7 +31,7 @@ export const HomeImage: FC<HomeImageProps> = ({
 
   return (
     <div
-      className="pointer-events-none absolute left-0 right-0 top-0 isolate z-0 h-[100dvh] overflow-hidden"
+      className="pointer-events-none absolute inset-x-0 top-0 isolate z-0 h-[100dvh] w-full overflow-hidden"
       aria-hidden
     >
       <Swiper
@@ -48,16 +48,21 @@ export const HomeImage: FC<HomeImageProps> = ({
           setActiveGameId(swiper.activeIndex);
         }}
         onSlideChange={onHeroMainSlideChange}
-        className="hero-home-swiper relative z-0 h-full min-h-[100dvh] w-full [&_.swiper-slide]:!h-full"
+        className="hero-home-swiper relative z-0 h-full min-h-[100dvh] w-full [&_.swiper-slide]:!h-full [&_.swiper-slide]:!w-full [&_.swiper-wrapper]:h-full [&_.swiper-wrapper]:w-full"
       >
         {gamesSlider.map((game) => (
-          <SwiperSlide key={game.id}>
-            <Image
-              imgUrl={game.background_image}
-              name={game.name}
-              effect="opacity"
-              styles="h-full min-h-[100dvh] w-full object-cover object-[center_20%]"
-            />
+          <SwiperSlide
+            key={game.id}
+            className="!box-border !h-full !w-full overflow-hidden"
+          >
+            <div className="relative h-full min-h-[100dvh] w-full [&_.lazy-load-image-background]:!absolute [&_.lazy-load-image-background]:!inset-0 [&_.lazy-load-image-background]:!block [&_.lazy-load-image-background]:!h-full [&_.lazy-load-image-background]:!w-full [&_.lazy-load-image-background]:!max-w-none [&_img]:!h-full [&_img]:!w-full [&_img]:!max-w-none [&_img]:!object-cover [&_img]:object-[center_20%]">
+              <Image
+                imgUrl={game.background_image}
+                name={game.name}
+                effect="opacity"
+                styles="h-full min-h-[100dvh] w-full max-w-none object-cover object-[center_20%]"
+              />
+            </div>
           </SwiperSlide>
         ))}
       </Swiper>
